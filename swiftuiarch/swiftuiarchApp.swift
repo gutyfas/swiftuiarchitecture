a29ac15b330004147ce356d93443b4f5845e637a
Agustin Palacios Reyes <apalaciosreyes@hotmail.om>
2023-05-30 16:25:11 -0400

    Initial Commit

---
+++ swiftuiarch/swiftuiarchApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftuiarchApp.swift
+//  swiftuiarch
+//
+//  Created by Agustin Palacios on 5/30/23.
+//
+
+import SwiftUI
+
+@main
+struct swiftuiarchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
